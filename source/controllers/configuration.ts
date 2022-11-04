f9bbd06f7fb60a56f7f50328402443d4a98883e8
mohsen <mbp.moh3n@gmail.com>
2022-11-04 21:13:38 +0330

    first commit

---
+++ source/controllers/configuration.ts
@@ -0,0 +1,58 @@
+/** source/controllers/configuration.ts */
+import { Request, Response, NextFunction } from 'express';
+import {NginxConfFile} from "nginx-conf";
+const filename = `${__dirname}/../files/readme.conf`;
+
+const getConfigurations = async (req: Request, res: Response, next: NextFunction) => {
+    NginxConfFile.create(filename, function (err, conf) {
+        return res.status(200).json({
+            message:  conf ? conf.nginx.http?.[0].server?.[0] : []
+        });
+    });
+
+};
+
+// adding a configuration
+const addConfiguration = async (req: Request, res: Response, next: NextFunction) => {
+    // get the data from req.body
+    let port: string = req.body.port;
+    let name: string = req.body.name;
+
+    NginxConfFile.create(filename, function (err, conf) {
+        console.log(conf?.nginx.http?.[0].location)
+        if (err || !conf) {
+            console.log(err);
+            return res.status(500).json({
+                message: 'Internal Server Error!'
+            });
+        }
+
+        const onFlushed = () => {
+            console.log('finished writing to disk');
+        };
+
+        conf.on('flushed', onFlushed);
+        const lastIndex = conf ? conf.nginx.http?.[0].location?.length : 0;
+        // blocks with values:
+        conf.nginx.http?.[0].server?.[0]._add('location', `/${name}`);
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_redirect', 'off');
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_pass', `http://127.0.0.1:${port}`);
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_http_version', '1.1');
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_set_header', 'Upgrade $http_upgrade');
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_set_header', 'Connection "upgrade"');
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_set_header', 'Host $http_host');
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_set_header', 'X-Real-IP $remote_addr');
+        conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add('proxy_set_header', 'X-Forwarded-For $proxy_add_x_forwarded_for');
+
+        conf.flush();
+        return res.status(200).json({
+            message: {
+                port, name
+            }
+        });
+    });
+
+
+};
+
+export default { getConfigurations, addConfiguration };
---
+++ source/routes/configuration.ts
@@ -0,0 +1,9 @@
+/** source/routes/configuration.ts */
+import express from 'express';
+import controller from '../controllers/configuration';
+const router = express.Router();
+
+router.get('/configuration', controller.getConfigurations);
+router.post('/configuration', controller.addConfiguration);
+
+export = router;
