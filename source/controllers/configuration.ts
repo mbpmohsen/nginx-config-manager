5489303d193429795bd130aa5a67bdfe58288f28
mohsen <mbp.moh3n@gmail.com>
2022-11-08 22:00:39 +0330

    fix routing problem by mamad

--- source/controllers/configuration.ts
+++ source/controllers/configuration.ts
@@ -5,75 +5,73 @@ const filename = `${__dirname}/../files/readme.conf`;
 const { exec } = require("child_process");
 
 const getConfigurations = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
   NginxConfFile.create(filename, function (err, conf) {
     return res.status(200).json({
-      message: conf ? conf.nginx.http?.[0].server?.[0] : [],
+      message: conf ? conf.nginx.server?.[0] : [],
     });
   });
 };
 
 // adding a configuration
 const addConfiguration = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
   // get the data from req.body
   let port: string = req.body.port;
   let name: string = req.body.name;
 
   NginxConfFile.create(filename, function (err, conf) {
-    console.log(conf?.nginx.http?.[0].location);
     if (err || !conf) {
       console.log(err);
       return res.status(500).json({
-        message: "Internal Server Error!",
-      });
+        message: "Internal Server Error!",});
     }
 
     const onFlushed = () => {
       console.log("finished writing to disk");
     };
 
-    conf.on("flushed", onFlushed);
-    const lastIndex = conf ? conf.nginx.http?.[0].location?.length : 0;
+    const lastIndex = conf ? conf.nginx.server?.[0].location?.length : 0;
     // blocks with values:
-    conf.nginx.http?.[0].server?.[0]._add("location", `/${name}`);
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_redirect",
-      "off"
+    conf.nginx.server?.[0]._add("location", `/${name}`);
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_redirect",
+        "off"
     );
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_pass",
-      `http://127.0.0.1:${port}`
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_pass",
+        `http://127.0.0.1:${port}`
     );
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_http_version",
-      "1.1"
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_http_version",
+        "1.1"
     );
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_set_header",
-      "Upgrade $http_upgrade"
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_set_header",
+        "Upgrade $http_upgrade"
     );
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_set_header",
-      'Connection "upgrade"'
+
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_set_header",
+        'Connection "upgrade"'
     );
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_set_header",
-      "Host $http_host"
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_set_header",
+        "Host $http_host"
     );
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_set_header",
-      "X-Real-IP $remote_addr"
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_set_header",
+        "X-Real-IP $remote_addr"
     );
-    conf.nginx.http?.[0].server?.[0].location?.[lastIndex as number]._add(
-      "proxy_set_header",
-      "X-Forwarded-For $proxy_add_x_forwarded_for"
+    conf.nginx.server?.[0].location?.[lastIndex as number]._add(
+        "proxy_set_header",
+        "X-Forwarded-For $proxy_add_x_forwarded_for"
     );
 
     conf.flush();
